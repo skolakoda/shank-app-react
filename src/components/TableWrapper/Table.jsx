70d87b5bc237cd4d86f31a7f131967746a9f0ddc
Damjan Pavlica <mudroljub@gmail.com>
2017-11-21 13:44:31 +0100

    narudzbine rade

--- src/components/TableWrapper/Table.jsx
+++ src/components/TableWrapper/Table.jsx
@@ -1,35 +1,25 @@
-import React from 'react';
+import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class Table extends React.Component {
-  constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
+const Table = props => {
+  var order = this.props.order
+  let narudzbina = []
+  for (var pice in order) {
+    narudzbina.push(<div key={pice}>{pice}: {order[pice]}</div>)
   }
-
-  handleClick() {
-    this.props.openMenu(this.props.tableNumber)
-  }
-
-  render() {
-    var order = this.props.order
-    let narudzbina = []
-    for (var pice in order) {
-      narudzbina.push(<div key={pice}>{pice}: {order[pice]}</div>)
-    }
-    return (
-      <div className="table" onClick={this.handleClick}>
-        <div className="broj-stola">{this.props.tableNumber}</div>
-        <div>{this.props.isActive && "aktivan"}</div>
-        {narudzbina}
-      </div>
-    )
-  }
+  return (
+    <div className="table" onClick={() => props.openMenu(props.tableNumber)}>
+      <div className="broj-stola">{this.props.tableNumber}</div>
+      <div>{this.props.isActive && "aktivan"}</div>
+      {narudzbina}
+    </div>
+  )
 }
 
 Table.propTypes = {
   isActive: PropTypes.bool.isRequired,
   tableNumber: PropTypes.number.isRequired,
   openMenu: PropTypes.func.isRequired,
-  // order: PropTypes.object.isRequired,
 }
+
+export default Table
